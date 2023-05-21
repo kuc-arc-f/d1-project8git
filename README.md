1f8cff4e05e9705a66e7b117ab522a41a0e0622a
中島浩二 <knaka0209@gmail.com>
2023-05-21 18:14:21 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+﻿# d1-project8git
+
+ Version: 0.9.1
+
+ Author  : Kouji Nakashima / kuc-arc-f.com
+
+ date    : 2023/05/20
+
+ update  :
+
+***
+### Summary
+
+cloudflare D1, workers sample
+
+***
+### blog 
+
+https://zenn.dev/knaka0209/books/b25ece7df93925
+
+***
+
---
+++ schema.sql
@@ -0,0 +1,50 @@
+DROP TABLE IF EXISTS Customers;
+CREATE TABLE Customers (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  createdAt TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updatedAt TIMESTAMP(3) NULL,
+  CompanyName TEXT NOT NULL,
+  ContactName TEXT NOT NULL
+);
+
+INSERT INTO Customers (CompanyName, ContactName) VALUES ('Alfreds Futterkiste', 'Maria Anders');
+INSERT INTO Customers (CompanyName, ContactName) VALUES ('Around the Horn', 'Thomas Hardy');
+INSERT INTO Customers (CompanyName, ContactName) VALUES ('Bs Beverages', 'Victoria Ashworth');
+
+DROP TABLE IF EXISTS todos;
+CREATE TABLE todos (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  createdAt TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updatedAt TIMESTAMP(3) NULL,
+  title TEXT NOT NULL,
+  content TEXT,
+  completed INTEGER  DEFAULT 0
+);
+DROP TABLE IF EXISTS Session;
+CREATE TABLE "Session" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "sessionId" TEXT NOT NULL,
+    "key" TEXT NOT NULL,
+    "value" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME
+);
+
+DROP TABLE IF EXISTS Repo;
+CREATE TABLE "Repo" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME
+);
+
+DROP TABLE IF EXISTS RepoCount;
+CREATE TABLE "RepoCount" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "count" INTEGER,
+    "uniques" INTEGER,
+    "repoId" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME
+);
+
